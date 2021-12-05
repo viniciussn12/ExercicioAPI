9a6626470170196b46a56017f650745fb2c8362a
Alba Lopes <albasandyra@gmail.com>
2021-12-05 09:53:58 -0300

    Atualizando o application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/projeto_apirest/';
+$config['base_url'] = 'https://projetoapirest.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
