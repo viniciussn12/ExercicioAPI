5ca84568c11236fcd6103eebeb91b45e06a36dab
Alba Lopes <albasandyra@gmail.com>
2021-12-05 15:23:14 -0300

    Atualizando o base_url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-//BASE_URL = "https://projetoapirest.herokuapp.com/"
-BASE_URL = "https://localhost/projeto_apirest/"
+BASE_URL = "https://projetoapirest.herokuapp.com/"
+//BASE_URL = "https://localhost/projeto_apirest/"
